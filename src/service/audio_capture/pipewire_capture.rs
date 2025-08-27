80aae8fa71b125249716895d18555156b57a3269
lcxl <lcx87654321@163.com>
2025-08-27 22:55:04 +0800

    update pipewire

--- src/service/audio_capture/pipewire_capture.rs
+++ src/service/audio_capture/pipewire_capture.rs
@@ -1,11 +1,18 @@
-use std::ffi::CString;
+use std::{ffi::CString, mem};
 
-use alsa::{
-    Direction, PCM, ValueOr,
-    device_name::HintIter,
-    pcm::{Access, Format, HwParams},
+use pipewire::{
+    context::Context,
+    core::Core,
+    main_loop::MainLoop,
+    properties::properties,
+    spa::{
+        param::{
+            format::{MediaSubtype, MediaType},
+            format_utils,
+        },
+        pod::Pod,
+    },
 };
-use pipewire::{context::Context, main_loop::MainLoop};
 
 use crate::{
     desk_error::DeskError,
@@ -29,37 +36,20 @@ impl PipewireAudioDeviceEnumerator {
 impl AudioDeviceEnumerator for PipewireAudioDeviceEnumerator {
     fn get_device_list(&self) -> Result<Vec<AudioDevice>, DeskError> {
         let mut audio_device_list = vec![];
-        let hint_iter = HintIter::new(None, &*CString::new("pcm").unwrap()).unwrap();
-        for hint in hint_iter {
-            log::info!("{:?}", hint);
-            audio_device_list.push(AudioDevice {
-                id: hint.name.unwrap_or_default(),
-                firendly_name: hint.desc.unwrap_or_default(),
-                data_flow: AudioDataFlow::Capture,
-                default: false,
-            });
-        }
 
         Ok(audio_device_list)
     }
 }
 
-pub struct PipewireAudioCapture {
-    pub pcm: PCM,
+struct UserData {
+    format: pipewire::spa::param::audio::AudioInfoRaw,
+    cursor_move: bool,
 }
+
+pub struct PipewireAudioCapture {}
 
 impl AudioCapture for PipewireAudioCapture {
     fn start(&mut self) -> Result<WaveFormat, DeskError> {
-        {
-            // For this example, we assume 44100Hz, one channel, 16 bit audio.
-            let hwp = HwParams::any(&self.pcm)?;
-            hwp.set_channels(1)?;
-            hwp.set_rate(44100, ValueOr::Nearest)?;
-            hwp.set_format(Format::s16())?;
-            hwp.set_access(Access::RWInterleaved)?;
-            self.pcm.hw_params(&hwp)?;
-        }
-        self.pcm.start()?;
         let wave_format = WaveFormat::default();
         Ok(wave_format)
     }
@@ -69,22 +59,156 @@ impl AudioCapture for PipewireAudioCapture {
     }
 
     fn stop(&mut self) -> Result<(), DeskError> {
-        self.pcm.drop()?;
         Ok(())
     }
 }
 
 impl PipewireAudioCapture {
+    // https://gitlab.freedesktop.org/pipewire/pipewire-rs/-/blob/main/pipewire/examples/audio-capture.rs?ref_type=heads
     pub fn new(desk_settings: &DeskSettings) -> Result<Self, DeskError> {
-        // 1. 创建主循环（glib main loop）
+        pipewire::init();
         let main_loop = MainLoop::new(None)?;
 
-        // 2. 创建 PipeWire 上下文并连接核心
         let context = Context::new(&main_loop)?;
-        let core = context.connect(None)?; // `None` 表示使用默认配置
+        let core = context.connect(None)?;
 
-        let pcm = PCM::new("default", Direction::Capture, false)?;
-        Ok(Self { pcm })
+        let data = UserData {
+            format: Default::default(),
+            cursor_move: false,
+        };
+
+        /* Create a simple stream, the simple stream manages the core and remote
+         * objects for you if you don't need to deal with them.
+         *
+         * If you plan to autoconnect your stream, you need to provide at least
+         * media, category and role properties.
+         *
+         * Pass your events and a user_data pointer as the last arguments. This
+         * will inform you about the stream state. The most important event
+         * you need to listen to is the process event where you need to produce
+         * the data.
+         */
+        let mut props = properties! {
+            *pipewire::keys::MEDIA_TYPE => "Audio",
+            *pipewire::keys::MEDIA_CATEGORY => "Capture",
+            *pipewire::keys::MEDIA_ROLE => "Music",
+        };
+        //if you want to capture from the sink monitor ports
+        props.insert(*pipewire::keys::STREAM_CAPTURE_SINK, "true");
+
+        let stream = pipewire::stream::Stream::new(&core, "audio-capture", props)?;
+
+        let _listener = stream
+            .add_local_listener_with_user_data(data)
+            .param_changed(|_, user_data, id, param| {
+                // NULL means to clear the format
+                let Some(param) = param else {
+                    return;
+                };
+                if id != pipewire::spa::param::ParamType::Format.as_raw() {
+                    return;
+                }
+
+                let (media_type, media_subtype) = match format_utils::parse_format(param) {
+                    Ok(v) => v,
+                    Err(_) => return,
+                };
+
+                // only accept raw audio
+                if media_type != MediaType::Audio || media_subtype != MediaSubtype::Raw {
+                    return;
+                }
+
+                // call a helper function to parse the format for us.
+                user_data
+                    .format
+                    .parse(param)
+                    .expect("Failed to parse param changed to AudioInfoRaw");
+
+                println!(
+                    "capturing rate:{} channels:{}",
+                    user_data.format.rate(),
+                    user_data.format.channels()
+                );
+            })
+            .process(|stream, user_data| match stream.dequeue_buffer() {
+                None => println!("out of buffers"),
+                Some(mut buffer) => {
+                    let datas = buffer.datas_mut();
+                    if datas.is_empty() {
+                        return;
+                    }
+
+                    let data = &mut datas[0];
+                    let n_channels = user_data.format.channels();
+                    let n_samples = data.chunk().size() / (mem::size_of::<f32>() as u32);
+
+                    if let Some(samples) = data.data() {
+                        if user_data.cursor_move {
+                            print!("\x1B[{}A", n_channels + 1);
+                        }
+                        println!("captured {} samples", n_samples / n_channels);
+                        for c in 0..n_channels {
+                            let mut max: f32 = 0.0;
+                            for n in (c..n_samples).step_by(n_channels as usize) {
+                                let start = n as usize * mem::size_of::<f32>();
+                                let end = start + mem::size_of::<f32>();
+                                let chan = &samples[start..end];
+                                let f = f32::from_le_bytes(chan.try_into().unwrap());
+                                max = max.max(f.abs());
+                            }
+
+                            let peak = ((max * 30.0) as usize).clamp(0, 39);
+
+                            println!(
+                                "channel {}: |{:>w1$}{:w2$}| peak:{}",
+                                c,
+                                "*",
+                                "",
+                                max,
+                                w1 = peak + 1,
+                                w2 = 40 - peak
+                            );
+                        }
+                        user_data.cursor_move = true;
+                    }
+                }
+            })
+            .register()?;
+
+        /* Make one parameter with the supported formats. The SPA_PARAM_EnumFormat
+         * id means that this is a format enumeration (of 1 value).
+         * We leave the channels and rate empty to accept the native graph
+         * rate and channels. */
+        let mut audio_info = pipewire::spa::param::audio::AudioInfoRaw::new();
+        audio_info.set_format(pipewire::spa::param::audio::AudioFormat::F32LE);
+        let obj = pipewire::spa::pod::Object {
+            type_: pipewire::spa::utils::SpaTypes::ObjectParamFormat.as_raw(),
+            id: pipewire::spa::param::ParamType::EnumFormat.as_raw(),
+            properties: audio_info.into(),
+        };
+        let values: Vec<u8> = pipewire::spa::pod::serialize::PodSerializer::serialize(
+            std::io::Cursor::new(Vec::new()),
+            &pipewire::spa::pod::Value::Object(obj),
+        )
+        .unwrap()
+        .0
+        .into_inner();
+
+        let mut params = [Pod::from_bytes(&values).unwrap()];
+
+        /* Now connect this stream. We ask that our process function is
+         * called in a realtime thread. */
+        stream.connect(
+            pipewire::spa::utils::Direction::Input,
+            None,
+            pipewire::stream::StreamFlags::AUTOCONNECT
+                | pipewire::stream::StreamFlags::MAP_BUFFERS
+                | pipewire::stream::StreamFlags::RT_PROCESS,
+            &mut params,
+        )?;
+
+        Ok(Self {})
     }
 }
 
