7cb7ebc3ac8aae5820b83af3b5168d53bb47868e
lcxl <lcx87654321@163.com>
2025-09-24 02:06:48 +0800

    update pipewire capture

--- src/service/audio_capture/pipewire_capture.rs
+++ src/service/audio_capture/pipewire_capture.rs
@@ -1,9 +1,8 @@
-use std::{ffi::CString, mem, sync::Arc, thread::JoinHandle};
+use std::{mem, thread::JoinHandle};
 
 use pipewire::{
     context::Context,
-    core::Core,
-    main_loop::{self, MainLoop},
+    main_loop::MainLoop,
     properties::properties,
     spa::{
         param::{
@@ -18,8 +17,7 @@ use crate::{
     desk_error::DeskError,
     model::{
         audio_capture::{
-            AudioBuffer, AudioCapture, AudioDataFlow, AudioDevice, AudioDeviceEnumerator,
-            WaveFormat,
+            AudioBuffer, AudioCapture, AudioDevice, AudioDeviceEnumerator, WaveFormat,
         },
         settings::DeskSettings,
     },
@@ -61,6 +59,8 @@ fn inner_pw_thread(
     main_sender: std::sync::mpsc::Sender<String>,
     pw_receiver: pipewire::channel::Receiver<PipewireCommand>,
 ) -> Result<(), DeskError> {
+    pipewire::init();
+
     let main_loop = MainLoop::new(None)?;
 
     let context = Context::new(&main_loop)?;
@@ -93,19 +93,31 @@ fn inner_pw_thread(
 
     let _listener = stream
         .add_local_listener_with_user_data(data)
-        .param_changed(|_, user_data, id, param| {
+        .state_changed(|_, user_data, old, new| {
+            log::info!("Stream state changed from {:?} to {:?}", old, new);
+        })
+        .control_info(|_, user_data, id: u32, control| {
+            log::info!("Stream control info, id: {}, control: {:?}", id, control);
+        })
+        .param_changed(|_, user_data, id: u32, param| {
             // NULL means to clear the format
             let Some(param) = param else {
                 return;
             };
-            if id != pipewire::spa::param::ParamType::Format.as_raw() {
-                return;
-            }
 
             let (media_type, media_subtype) = match format_utils::parse_format(param) {
                 Ok(v) => v,
                 Err(_) => return,
             };
+            log::info!(
+                "Stream param changed, id: {}, media type: {:?}, media sub type: {:?}",
+                id,
+                media_type,
+                media_subtype
+            );
+            if id != pipewire::spa::param::ParamType::Format.as_raw() {
+                return;
+            }
 
             // only accept raw audio
             if media_type != MediaType::Audio || media_subtype != MediaSubtype::Raw {
@@ -264,7 +276,11 @@ impl Drop for PipewireLoop {
     }
 }
 
-pub struct PipewireAudioCapture {}
+pub struct PipewireAudioCapture {
+    pub pipewire_loop: PipewireLoop,
+    pub main_receiver: std::sync::mpsc::Receiver<String>,
+    pub pw_sender: pipewire::channel::Sender<PipewireCommand>,
+}
 
 impl AudioCapture for PipewireAudioCapture {
     fn start(&mut self) -> Result<WaveFormat, DeskError> {
@@ -282,157 +298,26 @@ impl AudioCapture for PipewireAudioCapture {
 }
 
 impl PipewireAudioCapture {
-    // https://gitlab.freedesktop.org/pipewire/pipewire-rs/-/blob/main/pipewire/examples/audio-capture.rs?ref_type=heads
     pub fn new(desk_settings: &DeskSettings) -> Result<Self, DeskError> {
-        pipewire::init();
-        let main_loop = MainLoop::new(None)?;
+        let (main_sender, main_receiver) = std::sync::mpsc::channel();
+        let (pw_sender, pw_receiver) = pipewire::channel::channel();
 
-        let context = Context::new(&main_loop)?;
-        let core = context.connect(None)?;
+        let pw_sender_clone = pw_sender.clone();
 
-        let data = UserData {
-            format: Default::default(),
-            cursor_move: false,
-        };
+        let pipewire_loop =
+            PipewireLoop::new(desk_settings, main_sender, pw_sender_clone, pw_receiver)?;
 
-        /* Create a simple stream, the simple stream manages the core and remote
-         * objects for you if you don't need to deal with them.
-         *
-         * If you plan to autoconnect your stream, you need to provide at least
-         * media, category and role properties.
-         *
-         * Pass your events and a user_data pointer as the last arguments. This
-         * will inform you about the stream state. The most important event
-         * you need to listen to is the process event where you need to produce
-         * the data.
-         */
-        let mut props = properties! {
-            *pipewire::keys::MEDIA_TYPE => "Audio",
-            *pipewire::keys::MEDIA_CATEGORY => "Capture",
-            *pipewire::keys::MEDIA_ROLE => "Music",
-        };
-        //if you want to capture from the sink monitor ports
-        props.insert(*pipewire::keys::STREAM_CAPTURE_SINK, "true");
-
-        let stream = pipewire::stream::Stream::new(&core, "audio-capture", props)?;
-
-        let _listener = stream
-            .add_local_listener_with_user_data(data)
-            .param_changed(|_, user_data, id, param| {
-                // NULL means to clear the format
-                let Some(param) = param else {
-                    return;
-                };
-                if id != pipewire::spa::param::ParamType::Format.as_raw() {
-                    return;
-                }
-
-                let (media_type, media_subtype) = match format_utils::parse_format(param) {
-                    Ok(v) => v,
-                    Err(_) => return,
-                };
-
-                // only accept raw audio
-                if media_type != MediaType::Audio || media_subtype != MediaSubtype::Raw {
-                    return;
-                }
-
-                // call a helper function to parse the format for us.
-                user_data
-                    .format
-                    .parse(param)
-                    .expect("Failed to parse param changed to AudioInfoRaw");
-
-                println!(
-                    "capturing rate:{} channels:{}",
-                    user_data.format.rate(),
-                    user_data.format.channels()
-                );
-            })
-            .process(|stream, user_data| match stream.dequeue_buffer() {
-                None => println!("out of buffers"),
-                Some(mut buffer) => {
-                    let datas = buffer.datas_mut();
-                    if datas.is_empty() {
-                        return;
-                    }
-
-                    let data = &mut datas[0];
-                    let n_channels = user_data.format.channels();
-                    let n_samples = data.chunk().size() / (mem::size_of::<f32>() as u32);
-
-                    if let Some(samples) = data.data() {
-                        if user_data.cursor_move {
-                            print!("\x1B[{}A", n_channels + 1);
-                        }
-                        println!("captured {} samples", n_samples / n_channels);
-                        for c in 0..n_channels {
-                            let mut max: f32 = 0.0;
-                            for n in (c..n_samples).step_by(n_channels as usize) {
-                                let start = n as usize * mem::size_of::<f32>();
-                                let end = start + mem::size_of::<f32>();
-                                let chan = &samples[start..end];
-                                let f = f32::from_le_bytes(chan.try_into().unwrap());
-                                max = max.max(f.abs());
-                            }
-
-                            let peak = ((max * 30.0) as usize).clamp(0, 39);
-
-                            println!(
-                                "channel {}: |{:>w1$}{:w2$}| peak:{}",
-                                c,
-                                "*",
-                                "",
-                                max,
-                                w1 = peak + 1,
-                                w2 = 40 - peak
-                            );
-                        }
-                        user_data.cursor_move = true;
-                    }
-                }
-            })
-            .register()?;
-
-        /* Make one parameter with the supported formats. The SPA_PARAM_EnumFormat
-         * id means that this is a format enumeration (of 1 value).
-         * We leave the channels and rate empty to accept the native graph
-         * rate and channels. */
-        let mut audio_info = pipewire::spa::param::audio::AudioInfoRaw::new();
-        audio_info.set_format(pipewire::spa::param::audio::AudioFormat::F32LE);
-        let obj = pipewire::spa::pod::Object {
-            type_: pipewire::spa::utils::SpaTypes::ObjectParamFormat.as_raw(),
-            id: pipewire::spa::param::ParamType::EnumFormat.as_raw(),
-            properties: audio_info.into(),
-        };
-        let values: Vec<u8> = pipewire::spa::pod::serialize::PodSerializer::serialize(
-            std::io::Cursor::new(Vec::new()),
-            &pipewire::spa::pod::Value::Object(obj),
-        )
-        .unwrap()
-        .0
-        .into_inner();
-
-        let mut params = [Pod::from_bytes(&values).unwrap()];
-
-        /* Now connect this stream. We ask that our process function is
-         * called in a realtime thread. */
-        stream.connect(
-            pipewire::spa::utils::Direction::Input,
-            None,
-            pipewire::stream::StreamFlags::AUTOCONNECT
-                | pipewire::stream::StreamFlags::MAP_BUFFERS
-                | pipewire::stream::StreamFlags::RT_PROCESS,
-            &mut params,
-        )?;
-
-        Ok(Self {})
+        Ok(Self {
+            pipewire_loop,
+            main_receiver,
+            pw_sender,
+        })
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::sync::Once;
+    use std::{sync::Once, thread, time::Duration};
 
     use log::LevelFilter;
 
@@ -447,9 +332,18 @@ mod tests {
     }
 
     #[test]
-    fn test_device_info() -> Result<(), DeskError> {
+    fn test_pipewire_loop() -> Result<(), DeskError> {
         initialize();
+        let desk_settings = DeskSettings::default();
+        let (main_sender, main_receiver) = std::sync::mpsc::channel();
+        let (pw_sender, pw_receiver) = pipewire::channel::channel();
 
+        let pw_sender_clone = pw_sender.clone();
+
+        let pipewire_loop =
+            PipewireLoop::new(&desk_settings, main_sender, pw_sender_clone, pw_receiver)?;
+
+        thread::sleep(Duration::from_secs(20));
         Ok(())
     }
 }
