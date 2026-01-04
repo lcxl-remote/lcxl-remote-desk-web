17e23fc6fdd24a12f5b54d1a11a7bbc6e85a3240
lcxl <lcx87654321@163.com>
2026-01-05 00:10:19 +0800

    move version to server-version package

---
+++ server-version/src/lib.rs
@@ -0,0 +1,2 @@
+/// The current Server API version supported by this library.
+pub static SERVER_API_VERSION: i32 = 1;
--- signal-facade/src/version.rs
+++
@@ -1,2 +0,0 @@
-/// The current Signal API version supported by this library.
-pub static SIGNAL_API_VERSION: i32 = 1;
