2560983a14e12c0075dab6df015417ea45a37b2a
lcxl <lcx87654321@163.com>
2026-03-17 08:44:04 +0800

    fix: align Kubb client generic types with Axios to fix frontend build error

--- vite-project/src/lib/kubb-client.ts
+++ vite-project/src/lib/kubb-client.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 import type { AxiosRequestConfig, AxiosResponse, AxiosError, AxiosInstance } from "axios";
 
-export const client = axios.create({});
+export const axiosInstance = axios.create({});
 
 // Intercept desktop custom RestResponse success=false
-client.interceptors.response.use(
+axiosInstance.interceptors.response.use(
     (response) => {
         if (response.data && typeof response.data === "object" && response.data.success === false) {
             return Promise.reject(new Error(response.data.message || "Request failed"));
@@ -16,9 +16,21 @@ client.interceptors.response.use(
     }
 );
 
+/**
+ * Kubb expects a client that matches this signature:
+ * <TData, TError, TVariables>(config: RequestConfig<TVariables>) => Promise<Response<TData>>
+ */
+export const client: Client = <TData = unknown, _TError = unknown, TVariables = unknown>(
+    config: RequestConfig<TVariables>
+): Promise<Response<TData>> => {
+    return axiosInstance.request<TData, Response<TData>, TVariables>(config);
+};
+
 export default client;
 
-export type RequestConfig<TData = unknown> = Record<string, any>;
+export type RequestConfig<TData = unknown> = AxiosRequestConfig<TData>;
 export type Response<TData = unknown> = AxiosResponse<TData>;
 export type ResponseErrorConfig<TError = unknown> = AxiosError<TError>;
-export type { AxiosInstance as Client };
+export type Client = <TData = unknown, TError = unknown, TVariables = unknown>(
+    config: RequestConfig<TVariables>
+) => Promise<Response<TData>>;
